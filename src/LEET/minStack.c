725a5050baf7077247069873d4049bf6a14c93a1
GeraldRyan <gerald.ryan40@yahoo.com>
2021-01-01 00:08:52 -0600

    Not sure what's going on with my pointers in my Leetcode assignment

--- src/LEET/minStack.c
+++ src/LEET/minStack.c
@@ -5,9 +5,9 @@
 typedef struct
 {
   int current_stack_size;
-  int *stack;
-  int *top_of_stack_ptr;
-  int *leading;
+  int *stack;            // base of stack
+  int *leading;          // extreme top, to place new int
+  int *top_of_stack_ptr; // base of top, to retrieve
 } MinStack;
 
 /** initialize your data structure here. */
@@ -16,7 +16,7 @@ MinStack *minStackCreate()
 {
   MinStack *stack = malloc(sizeof(MinStack));
   stack->current_stack_size = 0;
-  stack->stack = malloc(sizeof(int));
+  stack->stack = malloc(sizeof(int) * 2);
   stack->top_of_stack_ptr = stack->stack;
   stack->leading = stack->stack;
   return stack;
@@ -26,17 +26,27 @@ void minStackPush(MinStack *obj, int x)
 {
   if (obj->current_stack_size == 0)
   {
-    *(obj->stack) = x;
+    *(obj->stack) = x; // creates seg fault
+    printf("Pushing\n");
+
     obj->current_stack_size++;
-    obj->leading += sizeof(int);
+    obj->leading = obj->stack + sizeof(int) * obj->current_stack_size;
+    // by default malloced to 1
+    // obj->stack = realloc(obj->stack, sizeof(int) * (obj->current_stack_size+1));
     return;
   }
-  else if(obj->current_stack_size > 0){
-    obj->current_stack_size++;
-    obj->stack = realloc(obj->stack, sizeof(int) * (obj->current_stack_size+1));
-    *(obj->leading) = x;
-    obj->leading += sizeof(int);
-    obj->top_of_stack_ptr += sizeof(int);
+  else if (obj->current_stack_size > 0)
+  {
+    obj->current_stack_size++;                                                 // from 2 on
+    obj->stack = realloc(obj->stack, sizeof(int) * (obj->current_stack_size)); // from 2 on
+    *(obj->leading) = x;                                                      // from top of 1 on
+    // obj->leading += sizeof(int); // is wrong. It's not referenced. Realloc will break this!
+    obj->leading = obj->stack + (sizeof(int) * obj->current_stack_size); // is right. It will stay associated to memory block of stack
+    obj->top_of_stack_ptr = obj->leading - sizeof(int);                  // this can be referenced by chain rule
+    if (x == -3)
+    {
+      printf("X is -3, so clearly obj->top_ptr = %d\n", obj->top_of_stack_ptr);
+    }
   }
   // printf("Memory address of stack %p and top of stack pointer %p\n", obj->stack, obj->top_of_stack_ptr);
   // printf("New Memory address of stack %p and top of stack pointer %p\n", obj->stack, obj->top_of_stack_ptr);
@@ -92,8 +102,7 @@ void print_state(MinStack *stack, char *op)
   printf("Stack Size:%d Last Operation: %s\n", stack->current_stack_size, op);
   for (int i = 0; i < stack->current_stack_size; i++)
   {
-    // int elevator = sizeof(int) *i;
-    int value = 1; //*((stack->stack) + sizeof(int) * i);
+    int value = *((stack->stack) + sizeof(int) * i);
     printf("%d:%d\n", i, value);
   }
   return;
@@ -106,6 +115,16 @@ int main(void)
   print_state(stack, "pushed -2");
   minStackPush(stack, 0);
   print_state(stack, "pushed 0");
+  minStackPush(stack, -3);
+  print_state(stack, "pushed -3");
+  minStackPush(stack, -3);
+  print_state(stack, "pushed -3");
+  minStackPush(stack, -3);
+  print_state(stack, "pushed -3");
+  minStackPush(stack, -3);
+  print_state(stack, "pushed -3");
+  minStackPush(stack, -3);
+  print_state(stack, "pushed -3");
   minStackPush(stack, -3);
   print_state(stack, "pushed -3");
   int min = minStackGetMin(stack);
